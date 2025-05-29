ff44b14f44de197c7dcd0c3d9f85d0f0928e6024
Martin Srsen <xsrsen1@fi.muni.cz>
2025-05-29 20:35:49 +0200

    remove ssl

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def plant_status(plant_id):
                          now=now)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=('cert.pem', 'key.pem')) 
+    app.run(host='0.0.0.0', port=5000, debug=True) 
