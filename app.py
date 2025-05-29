67b5822798ebab2a71e8d565ff171a03486f5b19
Martin Srsen <xsrsen1@fi.muni.cz>
2025-05-29 20:37:26 +0200

    limit to local host

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def plant_status(plant_id):
                          now=now)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False) 
+    app.run(host='127.0.0.1', port=5000, debug=False) 
