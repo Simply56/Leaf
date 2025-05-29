fab72f1f11b16a37c88fe752b78e4c6116e4b62a
Martin Srsen <xsrsen1@fi.muni.cz>
2025-05-29 20:36:50 +0200

    disable debugging

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def plant_status(plant_id):
                          now=now)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True) 
+    app.run(host='0.0.0.0', port=5000, debug=False) 
