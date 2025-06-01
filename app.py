45baa62c07a0a6853396b073f7cfd2cb367292dd
Martin Srsen <xsrsen1@fi.muni.cz>
2025-06-01 20:48:04 +0200

    remove unused import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, request, redirect, url_for
 import json
 from datetime import datetime, date
 import os
